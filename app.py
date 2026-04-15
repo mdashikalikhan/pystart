43f2dd3874e305bc5421ea7fbe54f98b7dcb4bc2
MD ASHIK ALI KHAN <khan.ashik@gmail.com>
2026-04-15 08:54:53 +0600

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+print("*" * 10)
+
+x = 1
+
+student_count = 1000
+
+print(student_count)
+
+course = "Python Programming"
+
+print(len(course))
+
+print(course[1])
+
+print(course[0:5])
