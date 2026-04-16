66feb74826bbc99fc666b94018d06bc9f30365e0
mdashikalikhan <khan.ashik@gmail.com>
2026-04-16 16:49:46 +0600

    python basic continued....

--- condition.py
+++ condition.py
@@ -7,3 +7,9 @@ elif temperature>20:
     print("fine")
 else:
     print("Cold")
+    
+age = 22
+
+message = "Eligible" if age>18 else "Not Eligible"
+
+print(message)
---
+++ loop.py
@@ -0,0 +1,5 @@
+for number in range(3):
+    print("Attempt ", number+1, "*" * (number+1))
+    
+for number in range(1, 4):
+    print("Attempt ", number, "*" * (number))    
